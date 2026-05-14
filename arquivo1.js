6667081dc531aaa602dfa21ea23be7954d49c83c
otavioHenrique245 <otavio.pimenta2000@gmail.com>
2026-05-14 14:25:09 -0300

    Update arquivo1.js

--- arquivo1.js
+++ arquivo1.js
@@ -1,20 +1,14 @@
 const express = require("express");
-
 const app = express();
 
+app.set("trust proxy", true);
+
 app.get("/", (req, res) => {
-
-    console.log("Recebeu acesso!");
-
     const ip =
-        req.headers["x-forwarded-for"] ||
+        req.headers["x-forwarded-for"]?.split(",")[0] ||
         req.socket.remoteAddress;
 
     console.log("IP:", ip);
 
-    res.redirect("https://discord.com");
-});
-
-app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+    res.send("OK");
 });
